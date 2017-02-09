45c73c1d475565c9bd9800f8b8685734f9d1c4cb
alanarcorrea <alanarcorrea@gmail.com>
2017-02-09 17:41:35 -0200

    Alteração no form

--- form.php
+++ form.php
@@ -16,17 +16,17 @@ require_once "conexao.php";
         <div class="form-group">
             <label for="title">Título: </label>
             <input type="text" class="form-control" id="title"
-                   name="title">
+                   name="title" required>
         </div>
 
         <div class="form-group">
             <label for="description">Descrição: </label>
-            <textarea class="form-control" id="description" name="description"></textarea>           
+            <textarea class="form-control" id="description" name="description" required></textarea>           
         </div>
 
         <div class="form-group">
             <label for="photo">Foto:</label>
-            <input type="text" class="form-control" id="photo"
+            <input type="file" class="form-control" id="photo"
                    name="photo">
         </div>
 
